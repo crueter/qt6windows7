b29e2e6c0c06805da67ebf2d6f64d97c5f6d9590
crueter <crueter@eden-emu.dev>
2026-01-02 17:04:33 -0500

    what about thi
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- qtbase/src/corelib/global/qlogging.cpp
+++ qtbase/src/corelib/global/qlogging.cpp
@@ -1520,8 +1520,9 @@ backtraceFramesForLogMessage(int frameCount,
     // This code is protected by QMessagePattern::mutex so it is thread safe on all compilers
     static const QRegularExpression rx(QStringLiteral("^(?:[^(]*/)?([^(/]+)\\(([^+]*)(?:[\\+[a-f0-9x]*)?\\) \\[[a-f0-9x]*\\]$"));
 
-    auto decodeFrame = [&](const void *&addr) -> DecodedFrame {
-        QScopedPointer<char*, QScopedPointerPodDeleter> strings(backtrace_symbols(&addr, 1));
+    auto decodeFrame = [&](const void *addr) -> DecodedFrame {
+        void *nonConstAddr = const_cast<void *>(addr);
+        QScopedPointer<char*, QScopedPointerPodDeleter> strings(backtrace_symbols(&nonConstAddr, 1));
         QString trace = QString::fromUtf8(strings.data()[0]);
         QRegularExpressionMatch m = rx.match(trace);
         if (!m.hasMatch())
